a7045970b9e22a48f2323d4c3edacf99216d630e
Hesam Sormeyli <hesam1385hs@gmail.com>
2026-07-06 20:00:00 +0330

    V0.3
    getComputerChoice done

--- files/script.js
+++ files/script.js
@@ -4,8 +4,14 @@
 //    IF number is from 5 to 8 return => paper
 //    IF number is from 9 to 12 return => scissors  
 
-function getComputerChoice () {
-    let number = (Math.floor(Math.random() *10) + 3);
-    return number; 
+let option1 = "rock";
+let option2 = "paper";
+let option3 = "scissors";
+function getComputerChoice() {
+    let number = (Math.floor(Math.random() * 10) + 3);
+    console.log(number);
+    if (number == 1 || number == 2 || number == 3 || number == 4) { return option1 }
+    else if (number == 5 || number == 6 || number == 7 || number == 8) { return option2 }
+    else if (number == 9 || number == 10 || number == 11 || number == 12) { return option3 }
 }
 console.log(getComputerChoice());
