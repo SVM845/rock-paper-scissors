737960669f798534a802d79f9be5ad695e543c06
Hesam Sormeyli <hesam1385hs@gmail.com>
2026-07-06 19:28:53 +0330

    V-0.2
    getComputerChoice pseudo code

--- files/script.js
+++ files/script.js
@@ -1,1 +1,9 @@
-console.log("Hello World")
+//write a function that returns one of the "rock", "paper", "scissors" strings randomly
+// use math.random to generate a number between 1 to 12
+//    IF number is from 1 to 4 return => rock
+//    IF number is from 5 to 8 return => paper
+//    IF number is from 9 to 12 return => scissors  
+
+function getComputerChoice () {
+    
+}
