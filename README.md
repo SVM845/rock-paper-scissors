3725b23590b9590a5abc2680d326b42eabb070fa
Hesam Sormeyli <hesam1385hs@gmail.com>
2026-08-10 11:58:15 +0330

    V-1.1
    action buttons

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 rock-paper-scissors is the first JS project of The Odin Project
 in this program I'll use different loops, conditions and most importantly, Logic.
+
+
 first part of this project is only JS with no UI ✅
 second part is adding UI and Styles using DOM manipulations and CSS ❌
--- files/script.js
+++ files/script.js
@@ -6,13 +6,30 @@
 //write a function to get the human choice
 //  USE prompt and store it in a variable
 //write playRound function such that it gets the two parameter's from previous funcs, plays the game and shows the winner with a string value
-//   
+// for buttons, HTMl is better
+// I want to work with JS so im creating and editing them here
 
+const btn1 = document.createElement('button');
+const btn2 = document.createElement('button');
+const btn3 = document.createElement('button');
+const main = document.querySelector("#main");
+const buttons = [btn1,btn2,btn3];
 let option1 = "rock";
 let option2 = "paper";
 let option3 = "scissors";
 let humanScore = 0;
 let computerScore = 0;
+
+buttons.forEach(button =>{
+    button.classList.add("button")
+})
+btn1.textContent = "Rock";
+btn2.textContent = "Paper";
+btn3.textContent = "Scissors";
+main.appendChild(btn1);
+main.appendChild(btn2);
+main.appendChild(btn3);
+
 
 function getComputerChoice() {
     let number = (Math.floor(Math.random() * 10) + 3);
@@ -55,19 +72,21 @@ function playRound(humanChoice, computerChoice) {
     }
 }
 
-function playGame() {
-    for (let i = 1; i <= 5; i++) {
-        const hc = getHumanChoice();
-        const cc = getComputerChoice();
-        console.log("\n Round ", i);
-        console.log(playRound(hc, cc));
-    }
-    getWinner(humanScore, computerScore);
-}
+// function playGame() {
+//     for (let i = 1; i <= 5; i++) {
+//         const hc = getHumanChoice();
+//         const cc = getComputerChoice();
+//         console.log("\n Round ", i);
+//         console.log(playRound(hc, cc));
+//     }
+//     getWinner(humanScore, computerScore);
+// }
 function getWinner(humanScore, computerScore) {
     console.log("Human Score: " + humanScore + " | " + "Computer Score: " + computerScore);
     if (humanScore > computerScore) { console.log("Human Won The Game, Score:  " + humanScore) }
     else if (computerScore > humanScore) { console.log("Computer Won The Game, Score: " + computerScore) }
     else { console.log("Game Is A Draw!") }
 }
-playGame();
+// playGame();
+
+
